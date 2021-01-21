14d300533833ec2bf9066885343cc4a052f413ac
Dmitrii Kurganskii <44755635+dkurganskii@users.noreply.github.com>
2021-01-21 10:18:28 -0600

    parse incoming events

--- comments/index.js
+++ comments/index.js
@@ -1,48 +1,47 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const {randomBytes} = require('crypto')
-const cors = require('cors')
-const axios = require('axios')
+const express = require('express');
+const bodyParser = require('body-parser');
+const { randomBytes } = require('crypto');
+const cors = require('cors');
+const axios = require('axios');
 
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
 
-const app = express()
-app.use(bodyParser.json())
-app.use(cors())
+const commentsByPostId = {};
 
-const commentsByPostId = {}
+app.get('/posts/:id/comments', (req, res) => {
+  res.send(commentsByPostId[req.params.id] || []);
+});
 
-app.get('/posts/:id/comments', (req, res)=>{
-res.send(commentsByPostId[req.params.id] || [])
-})
+app.post('/posts/:id/comments', async (req, res) => {
+  const commentId = randomBytes(4).toString('hex');
+  const { content } = req.body;
 
-app.post('/posts/:id/comments', async (req, res)=>{
-    const commentId = randomBytes(4).toString('hex')
-    const {content} = req.body
+  const comments = commentsByPostId[req.params.id] || [];
 
-const comments = commentsByPostId[req.params.id] || []
+  comments.push({ id: commentId, content });
 
-comments.push({id: commentId, content})
+  commentsByPostId[req.params.id] = comments;
 
-commentsByPostId[req.params.id] = comments
-
-await axios.post('http://localhost:4005/events',{
+  await axios.post('http://localhost:4005/events', {
     type: 'CommentCreated',
-    data:{
-        id: commentId,
-        content,
-        postId: req.params.id
+    data: {
+      id: commentId,
+      content,
+      postId: req.params.id
     }
-})
+  });
 
-res.status(201).send(comments)
-})
+  res.status(201).send(comments);
+});
 
-app.post('/events', (req, res)=>{
-    console.log('Received Event', req.body.type)
+app.post('/events', (req, res) => {
+  console.log('Event Received:', req.body.type);
 
-    res.send({})
-})
+  res.send({});
+});
 
-app.listen(4001, ()=>{
-    console.log('Listening on 4001')
-})
+app.listen(4001, () => {
+  console.log('Listening on 4001');
+});
