a437f598b451a9b5efad45cb8c1baab72d5e5247
Dmitrii Kurganskii <44755635+dkurganskii@users.noreply.github.com>
2021-01-22 10:20:37 -0600

    add comment moderation

--- query/index.js
+++ query/index.js
@@ -8,27 +8,27 @@ app.use(cors())
 
 const posts = {}
 
-app.get('/posts', (req, res)=>{
-res.send(posts)
+app.get('/posts', (req, res) => {
+    res.send(posts)
 })
 
-app.post('/events', (req, res)=>{
-    const {type, data} = req.body
+app.post('/events', (req, res) => {
+    const { type, data } = req.body
 
-    if(type === 'PostCreated'){
-       const {id, title} = data
-       posts[id] = {id, title, comments: []}
+    if (type === 'PostCreated') {
+        const { id, title } = data
+        posts[id] = { id, title, comments: [] }
     }
 
-    if(type === 'CommentCreated'){
-        const {id, content, postId} = data
+    if (type === 'CommentCreated') {
+        const { id, content, postId, status } = data
         const post = posts[postId]
-        post.comments.push({id, content})
+        post.comments.push({ id, content, status })
     }
-// console.log(posts)
+    // console.log(posts)
     res.send({})
 })
 
-app.listen(4002, ()=>{
+app.listen(4002, () => {
     console.log('Listening on 4002')
 })
